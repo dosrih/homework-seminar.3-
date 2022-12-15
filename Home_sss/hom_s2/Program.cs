34d501e6a333e394481facac99311adb872a954e
Doston <119062575+dosrih@users.noreply.github.com>
2022-12-15 22:03:33 +0500

    Add files via upload

---
+++ Home_sss/hom_s2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int x1 = ReadInt("Enter the X coordinate of the first point: ");
+int y1 = ReadInt("Enter the Y coordinate of the first point: ");
+int z1 = ReadInt("Enter the Z coordinate of the first point: ");
+int x2 = ReadInt("Enter the X coordinate of the first point: ");
+int y2 = ReadInt("Enter the Y coordinate of the first point: ");
+int z2 = ReadInt("Enter the Z coordinate of the first point: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Length of the segment {length}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
