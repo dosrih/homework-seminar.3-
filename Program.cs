546971e4c33e4d2d59c2cd25d058a00f4a5c02c2
doston <rikhsiboevdoston@email.com>
2022-12-15 22:00:50 +0500

    .

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter a number: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Palindrome");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - NOT A palindrome");
+    }
+}
+else
+{
+    Console.WriteLine($"ERROR: {number} is not a five-digit");
+}
